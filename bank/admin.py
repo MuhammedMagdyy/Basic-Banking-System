0f87e83018447d3e7ff57dc90b2a7726a830a012
MuhammedMagdyy <Mohamedmody04@gmail.com>
2023-07-02 20:08:50 +0300

    add sqlite3

--- bank/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from bank.models import *
-# Register your models here.
-
-admin.site.register(Profile)
--- bank/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from bank.views import *
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-urlpatterns = [
-    path('index/', index_view, name="index"),
-    path('list/', list_view, name='list'),
-    path('form/<id>', update_view, name="update"),
-]
